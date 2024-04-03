563628e694e3371100dacb0bd0b864b31ce877ba
stone <stoneUoU@163.com>
2024-04-03 14:52:31 +0800

    fix:1.修复方法调用出错

--- Hi-Personal-Module/Classes/Controller/HiPersonalViewController.swift
+++ Hi-Personal-Module/Classes/Controller/HiPersonalViewController.swift
@@ -47,7 +47,7 @@ class HiPersonalViewController:UIViewController {
         
     // 当时图已经消失时调用该方法
     override func viewDidDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+        super.viewDidDisappear(animated)
     }
         
     // 当接收到内存警告时会执行这个方法
